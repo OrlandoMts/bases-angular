db5b22305b9dfdaddf786aea5ba8964ff26180a7
OrlandoMts <dani_monts19@hotmail.com>
2022-08-14 21:01:07 -0500

    iniciando curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01-bases';
+
+  contador: number = 0;
+  base    : number = 5;
+
+  // constructor(){};
+
+  acomular( valor: number): void {
+    this.contador += valor;
+  }
 }
